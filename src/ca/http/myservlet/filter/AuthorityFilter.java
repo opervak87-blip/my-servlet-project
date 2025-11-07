4601ba665f99c6f7ca156ab06d9e3430b5a62098
opervak87-blip <o.pervak87@gmail.com>
2025-11-07 13:06:44 -0600

    fix filter

--- src/ca/http/myservlet/filter/AuthorityFilter.java
+++ src/ca/http/myservlet/filter/AuthorityFilter.java
@@ -1,6 +1,7 @@
 package ca.http.myservlet.filter;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,23 +13,36 @@ import javax.servlet.http.HttpServletResponse;
 
 import ca.http.myservlet.bean.User;
 import ca.http.myservlet.config.AppConstants;
+import ca.http.myservlet.service.impl.UserServiceImpl;
 
-public class AuthorityFilter implements Filter{
+public class AuthorityFilter implements Filter {
+	
+	private static final Logger log = Logger.getLogger(AuthorityFilter.class.getName());
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpRespose = (HttpServletResponse) response;
 		
-		User user = (User) httpRequest.getSession().getAttribute(AppConstants.USER.get());
+		 String uri = httpRequest.getRequestURI();
+		 String command = httpRequest.getParameter("command");
+		 
+		 log.info ("URI : " + uri + "  COMMAND : " + command);
 		
+		if (!uri.contains("user")) {
+		        chain.doFilter(request, response);
+		        return;
+		    }
+
+		User user = (User) httpRequest.getSession().getAttribute(AppConstants.USER.get());
+
 		if (user == null) {
 			httpRespose.sendRedirect(httpRequest.getContextPath() + "/Controller?command=goToIndexPage");
 			return;
-		}
-		
-		chain.doFilter(httpRequest, httpRespose);
+		} 
+			chain.doFilter(request, response);
 		
 	}
 
